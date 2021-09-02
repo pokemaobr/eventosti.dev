7b94b75e50e6f4cc8330ebf8e42ef4fb152dd48c
Pokemaobr <contato@pokemaobr.dev>
2021-09-02 15:55:49 -0300

    Primeiro commit

---
+++ public/assets/js/main.js
@@ -0,0 +1,130 @@
+"use strict";
+
+/* ======= Header animation ======= */   
+const header = document.getElementById('header');  
+
+window.onload=function() 
+{   
+    headerAnimation(); 
+
+};
+
+window.onresize=function() 
+{   
+    headerAnimation(); 
+
+}; 
+
+window.onscroll=function() 
+{ 
+    headerAnimation(); 
+
+}; 
+    
+
+function headerAnimation () {
+
+    var scrollTop = window.scrollY;
+	
+	if ( scrollTop > 100 ) {	    
+	    header.classList.add('header-shrink');    
+	    	    
+	} else {
+	    header.classList.remove('header-shrink');
+	}
+
+};
+
+/* ===== Smooth scrolling ====== */
+/*  Note: You need to include smoothscroll.min.js (smooth scroll behavior polyfill) on the page to cover some browsers */
+/* Ref: https://github.com/iamdustan/smoothscroll */
+
+
+let scrollLinks = document.querySelectorAll('.scrollto');
+const pageNavWrapper = document.getElementById('navigation');
+
+scrollLinks.forEach((scrollLink) => {
+
+	scrollLink.addEventListener('click', (e) => {
+		
+		e.preventDefault();
+
+		let element = document.querySelector(scrollLink.getAttribute("href"));
+		
+		const yOffset = 69; //page .header height
+		
+		//console.log(yOffset);
+		
+		const y = element.getBoundingClientRect().top + window.pageYOffset - yOffset;
+		
+		window.scrollTo({top: y, behavior: 'smooth'});
+		
+		
+		//Collapse mobile menu after clicking
+		if (pageNavWrapper.classList.contains('show')){
+			pageNavWrapper.classList.remove('show');
+		}
+
+		
+    });
+	
+});
+    
+
+/* ===== Gumshoe SrollSpy ===== */
+/* Ref: https://github.com/cferdinandi/gumshoe  */
+// Get the sticky header
+
+
+// Initialize Gumshoe
+var spy = new Gumshoe('#navigation a', {
+	offset: 69 //page .header heights
+});
+
+
+/* ======= Countdown ========= */
+// set the date we're counting down to
+//var target_date = new Date("Oct 12, 2024").getTime();
+ 
+// variables for time units
+var days, hours, minutes, seconds;
+ 
+// get tag element
+var countdown =  document.getElementById("countdown-box");
+var days_span = document.createElement("SPAN");
+days_span.className = 'days';
+countdown.appendChild(days_span);
+var hours_span = document.createElement("SPAN");
+hours_span.className = 'hours';
+countdown.appendChild(hours_span);
+var minutes_span = document.createElement("SPAN");
+minutes_span.className = 'minutes';
+countdown.appendChild(minutes_span);
+var secs_span = document.createElement("SPAN");
+secs_span.className = 'secs';
+countdown.appendChild(secs_span);
+ 
+// update the tag with id "countdown" every 1 second
+setInterval(function () {
+ 
+    // find the amount of "seconds" between now and target
+    var current_date = new Date().getTime();
+    var seconds_left = (target_date - current_date) / 1000;
+ 
+    // do some time calculations
+    days = parseInt(seconds_left / 86400);
+    seconds_left = seconds_left % 86400;
+     
+    hours = parseInt(seconds_left / 3600);
+    seconds_left = seconds_left % 3600;
+     
+    minutes = parseInt(seconds_left / 60);
+    seconds = parseInt(seconds_left % 60);
+     
+    // format countdown string + set tag value.
+    days_span.innerHTML = '<span class="number">' + days + '</span>' + '<span class="unit">Dias</span>';
+    hours_span.innerHTML = '<span class="number">' + hours + '</span>' + '<span class="unit">Hrs</span>';
+    minutes_span.innerHTML = '<span class="number">' + minutes + '</span>' + '<span class="unit">Mins</span>';
+    secs_span.innerHTML = '<span class="number">' + seconds + '</span>' + '<span class="unit">Segs</span>';
+ 
+}, 1000);
---
+++ src/Controller/IndexController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Eventos;
+
+class IndexController extends AbstractController
+{
+
+    #[Route('/', name: 'index')]
+    public function index(): Response
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $eventos = $entityManager->getRepository('App\\Entity\\Eventos')->findBy(['habilitado' => 1], ['dataInicio' => 'asc', 'dataFim' => 'asc']);
+
+        return $this->render('index.html.twig', ['eventos' => $eventos, 'tipo' => [0 => 'Online',1 => 'Presencial', 2 => 'Híbrido']]);
+
+    }
+
+    #[Route('/cadastrar', name: 'cadastrar')]
+    public function cadastrar(): Response
+    {
+        return $this->render('cadastrar.html.twig');
+    }
+
+    #[Route('/cadastro', name: 'cadastro', methods: ['POST'])]
+    public function cadastro(Request $request): Response
+    {
+
+        $entityManager = $this->getDoctrine()->getManager();
+
+        try {
+            $eventos = new Eventos;
+            $eventos->setNome($request->request->get('nome'));
+            $eventos->setTipo($request->request->get('tipo'));
+            $eventos->setLocal($request->request->get('local'));
+            $eventos->setImagem($request->request->get('imagem'));
+            $eventos->setDescricao($request->request->get('descricao'));
+            $eventos->setDataInicio(new \DateTime($request->request->get('dataInicio')));
+            $eventos->setDataFim(new \DateTime($request->request->get('dataFim')));
+            $eventos->setLink($request->request->get('link'));
+            if (!empty($request->request->get('twitter'))) {
+                $eventos->setTwitter($request->request->get('twitter'));
+            }
+            if (!empty($request->request->get('instagram'))) {
+                $eventos->setInstagram($request->request->get('instagram'));
+            }
+            if (!empty($request->request->get('outro'))) {
+                $eventos->setOutro($request->request->get('outro'));
+            }
+            $eventos->setHabilitado(0);
+
+
+            $entityManager->persist($eventos);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('cadastrar',['status' => 1]);
+
+        } catch (Exception) {
+            return $this->redirectToRoute('cadastrar',['status' => 2]);
+        }
+
+
+        return $this->render('cadastrar.html.twig',['status' => 2]);
+    }
+
+    #[Route('/upload', name: 'upload', methods: ['POST'])]
+    public function upload(Request $request): Response
+    {
+
+        $image = $request->request->get('image');
+        if (isset($image)) {
+            $data = $image;
+
+            $image_array_1 = explode(";", $data);
+
+            $image_array_2 = explode(",", $image_array_1[1]);
+
+            $data = base64_decode($image_array_2[1]);
+
+            $imageName = time() . '.png';
+
+            $root = $this->getParameter('kernel.project_dir');
+
+            file_put_contents($root . DIRECTORY_SEPARATOR . 'public' . DIRECTORY_SEPARATOR . 'assets' . DIRECTORY_SEPARATOR . 'images' . DIRECTORY_SEPARATOR . 'events' . DIRECTORY_SEPARATOR . $imageName, $data);
+
+        }
+
+        return $this->render('upload.html.twig', ['imageName' => $imageName]);
+    }
+}

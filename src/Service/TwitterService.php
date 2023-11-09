123f42a74ec4100d27b2ec1a182ee73c619c00e3
Pokemaobr <contato@pokemaobr.dev>
2023-11-09 16:32:53 -0300

    Reativando o envio de tweet assim que o evento for confirmado e atualizando coisas na index

--- src/Service/TwitterService.php
+++ src/Service/TwitterService.php
@@ -3,14 +3,15 @@
 namespace App\Service;
 
 use App\Entity\Eventos;
-use DG\Twitter\Twitter;
+use Abraham\TwitterOAuth\TwitterOAuth;
 
 class TwitterService {
 
-    private Twitter $twitter;
+    private TwitterOAuth $twitter;
 
     public function __construct() {
-        $this->twitter = new Twitter($_ENV['TWITTER_API_KEY'],$_ENV['TWITTER_API_SECRET'],$_ENV['TWITTER_ACCESS_TOKEN'],$_ENV['TWITTER_TOKEN_SECRET']);
+        $this->twitter = new TwitterOAuth($_ENV['TWITTER_API_KEY'],$_ENV['TWITTER_API_SECRET'],$_ENV['TWITTER_ACCESS_TOKEN'],$_ENV['TWITTER_TOKEN_SECRET']);
+        $this->twitter->setApiVersion('2');
     }
 
     public function enviaMensagemCadastroEvento(Eventos $evento) {
@@ -18,10 +19,11 @@ class TwitterService {
         $mensagem = 'Mais um evento cadastrado na plataforma https://eventosti.dev confira agora! ' .$evento->getNome() . ' em: ' . $evento->getLink();
 
         try {
-        $this->twitter->send($mensagem);
+        //$this->twitter->send($mensagem);
+        $tweet = $this->twitter->post('tweets',['text' => $mensagem]);
         }
         catch (Exception $e) {
-
+           dd($e->getMessage());
         }
     }
 
